ea1781e0dbeb214c352548da5f5ae5d738426667
hecalu <hecalu@gmail.com>
2018-11-11 19:40:01 +0100

    Remove useless pokemon search feature in top menu.

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,18 +1,3 @@
 $(document).ready(function(){
   
-	/**
-   * Top navbar pokemon search form.
-   * Use a datalist to autocomplete and suggest pokemon names to user.
-   * On submit search, opens a new page on pokepedia with the selected pokemon.
-   */
-  $('.view-pokemon-details').on('submit', function(e){
-		e.preventDefault();
-		var pokemonName = $('.view-pokemon').val();
-		var $pokemonSearched = $(this).find('option[value="' + pokemonName + '"]');
-		if($pokemonSearched.length > 0) {
-			// Redirect to pokepedia
-			window.open('http://www.pokepedia.fr/'+pokemonName, '_blank');
-		}
-	});
-  
 });
